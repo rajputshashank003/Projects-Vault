142b37ee05a9be0541c6a1bd540eb8575977d1a3
1rajputshashank@gmail.com <hariomshashank@gmail.com>
2024-12-28 15:56:13 +0530

    fix width of image

--- src/ReviewComponent.jsx
+++ src/ReviewComponent.jsx
@@ -18,7 +18,7 @@ function ReviewComponent() {
     return (
         <div className='flex flex-col max-md:p-8 justify-center items-center min-h-screen h-fit bg-neutral-900'>
             <div className='w-[80%] grid grid-cols-2 max-md:flex max-md:flex-col gap-20 text-4xl '>
-                <div className='col-span-1 relative h-80 w-[110%]'>
+                <div className='col-span-1 relative h-80 w-full '>
                     <Link to={data[active].link ?? "/"}>
                         <AnimatePresence>
                         {
