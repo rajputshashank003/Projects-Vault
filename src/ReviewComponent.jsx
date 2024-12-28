3d4222690393f94fdbda857cb9cb5105535ead35
1rajputshashank@gmail.com <hariomshashank@gmail.com>
2024-12-28 15:49:10 +0530

    add my projects as data in component

--- src/ReviewComponent.jsx
+++ src/ReviewComponent.jsx
@@ -3,6 +3,7 @@ import { IconArrowLeft, IconArrowRight } from '@tabler/icons-react';
 import { AnimatePresence, motion } from "framer-motion";
 import TypingEffect from './TypingEffect';
 import {data} from "./data.js";
+import { Link } from "react-router-dom";
 
 function ReviewComponent() {
     const [active , setActive ] = useState(0);
@@ -16,44 +17,46 @@ function ReviewComponent() {
 
     return (
         <div className='flex flex-col max-md:p-8 justify-center items-center min-h-screen h-fit bg-neutral-900'>
-            <div className='w-[70%] grid grid-cols-2 max-md:flex max-md:flex-col gap-12 text-4xl '>
-                <div className='col-span-1 relative h-80 w-full'>
-                    <AnimatePresence>
-                    {
-                        data.map((el, ind) => (
-                        <motion.div
-                        key={ind}
-                        initial={{
-                            opacity : 0,
-                            scale : 0.9,
-                            rotate : randomRotate(),
-                            zIndex : -100,
-                            y : 0
-                        }}
-                        animate={{
-                            opacity : active == ind ? 1 : 0.7,
-                            scale : active == ind ? 1 : 0.9,
-                            rotate : active == ind ? 0 : randomRotate(),
-                            zIndex : active == ind ? 9999 : ind,
-                            y : active == ind ? [0, -80 , 0] : 0
-                        }}
-                        exit={{
-                            opacity : 0,
-                            rotate : randomRotate(),
-                            scale : 0.9,
-                            y : 0
-                        }}
-                        transition={{
-                            duration : 0.4,
-                            ease : "easeInOut"
-                        }}
-                        className='absolute inset-0 origin-bottom '
-                        >
-                            <img src={el.img} className='rounded-xl h-72 max-md:h-64' />
-                        </motion.div>
-                        ))
-                    }
-                    </AnimatePresence>
+            <div className='w-[80%] grid grid-cols-2 max-md:flex max-md:flex-col gap-20 text-4xl '>
+                <div className='col-span-1 relative h-80 w-[110%]'>
+                    <Link to={data[active].link ?? "/"}>
+                        <AnimatePresence>
+                        {
+                            data.map((el, ind) => (
+                            <motion.div
+                            key={ind}
+                            initial={{
+                                opacity : 0,
+                                scale : 0.9,
+                                rotate : randomRotate(),
+                                zIndex : -100,
+                                y : 0
+                            }}
+                            animate={{
+                                opacity : active == ind ? 1 : 0.7,
+                                scale : active == ind ? 1 : 0.9,
+                                rotate : active == ind ? 0 : randomRotate(),
+                                zIndex : active == ind ? 9999 : ind,
+                                y : active == ind ? [0, -80 , 0] : 0
+                            }}
+                            exit={{
+                                opacity : 0,
+                                rotate : randomRotate(),
+                                scale : 0.9,
+                                y : 0
+                            }}
+                            transition={{
+                                duration : 0.4,
+                                ease : "easeInOut"
+                            }}
+                            className='absolute w-full inset-0 origin-bottom '
+                            >
+                                <img src={el.img} className='rounded-xl h-72 max-md:h-64' />
+                            </motion.div>
+                            ))
+                        }
+                        </AnimatePresence>
+                    </Link>
                 </div>
                 <div className='col-span-1 flex flex-col'>
                     <motion.div
@@ -71,15 +74,17 @@ function ReviewComponent() {
                         ease : "easeInOut"
                         }}
                     >
-                        <h2 className='text-bolder text-gray-300'>
-                        {data[active].title}
-                        </h2>
-                        <div className=''>
-                        <TypingEffect text={data[active].description}/>
-                        </div>
+                        <Link to={data[active].link ?? "/"}>
+                            <h2 className='text-bolder hover:scale-110 hover:font-bold duration-200 text-gray-300'>
+                                {data[active].title}
+                            </h2>
+                            <div className=''>
+                                <TypingEffect text={data[active].description}/>
+                            </div>
+                        </Link>
                         <div className='flex justify-center gap-16 w-full mt-10'>
-                        <IconArrowLeft onClick={handlePrev} color='white' className="bg-gray-400 h-8 hover:scale-110 duration-150 rotate-[20deg] w-8 p-1 rounded-full cursor-pointer" />
-                        <IconArrowRight onClick={handleNext} color='white' className="bg-gray-400 h-8 hover:scale-110 duration-150 rotate-[-20deg] w-8 p-1 rounded-full cursor-pointer" />
+                            <IconArrowLeft onClick={handlePrev} color='white' className="bg-gray-400 h-8 hover:scale-110 duration-150 rotate-[20deg] w-8 p-1 rounded-full cursor-pointer" />
+                            <IconArrowRight onClick={handleNext} color='white' className="bg-gray-400 h-8 hover:scale-110 duration-150 rotate-[-20deg] w-8 p-1 rounded-full cursor-pointer" />
                         </div>
                     </motion.div>
                 </div>
--- src/data.js
+++ src/data.js
@@ -1,27 +1,31 @@
 export const data = [
-    {
-      img : "https://images.unsplash.com/photo-1498887960847-2a5e46312788?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2FyfGVufDB8fDB8fHww",
-      title : "abc def",
-      description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis rerum dolores non aut eius quasi dolorum accusamus hic provident fuga saepe dignissimos, aliquid ullam quas, nostrum voluptate. Dolore, neque sit!"
-    },
-    {
-      img : "https://images.unsplash.com/photo-1610647752706-3bb12232b3ab?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fGNhcnxlbnwwfHwwfHx8MA%3D%3D",
-      title : "abc fds",
-      description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis rerum dolores non aut eius quasi dolorum accusamus hic provident fuga saepe dignissimos, aliquid ullam quas, nostrum voluptate. Dolore, neque sit!"
-    },
-    {
-      img : "https://images.unsplash.com/photo-1490902931801-d6f80ca94fe4?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGNhcnxlbnwwfHwwfHx8MA%3D%3D",
-      title : "abc asdf",
-      description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis rerum dolores non aut eius quasi dolorum accusamus hic provident fuga saepe dignissimos, aliquid ullam quas, nostrum voluptate. Dolore, neque sit!"
-    },
-    {
-      img : "https://images.unsplash.com/photo-1486262715619-67b85e0b08d3?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDZ8fGNhcnxlbnwwfHwwfHx8MA%3D%3D",
-      title : "abc asdf",
-      description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis rerum dolores non aut eius quasi dolorum accusamus hic provident fuga saepe dignissimos, aliquid ullam quas, nostrum voluptate. Dolore, neque sit!"
-    },
-    {
-      img : "https://images.unsplash.com/photo-1556448851-9359658faa54?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjJ8fGNhcnxlbnwwfHwwfHx8MA%3D%3D",
-      title : "abc asdfsdfer",
-      description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis rerum dolores non aut eius quasi dolorum accusamus hic provident fuga saepe dignissimos, aliquid ullam quas, nostrum voluptate. Dolore, neque sit!"
-    }
-  ]
+  {
+    img : "https://github.com/rajputshashank003/Chess.v.0.2.1/blob/main/image.png?raw=true",
+    title : "ChessV",
+    link : "https://chessv.netlify.app",
+    description : "ChessV is an online chess game build using React, Node.js, chess.js library , Socket.io for real-time gameplay and webRTC for calling.",
+  },
+  {
+    img : "https://github.com/rajputshashank003/Airbnb-MajorProject/raw/main/image.png",
+    title : "Airbnb",
+    link : "https://1airbnb.vercel.app",
+    description : "Welcome to the Airbnb Clone project, a web application built to replicate key features of Airbnb using EJS for the frontend, Node.js and Express.js for the backend, MongoDB for the database, Cloudinary for saving image data and Bing Maps API to display listing locations."
+  },
+  {
+    img : "https://images.unsplash.com/photo-1498887960847-2a5e46312788?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2FyfGVufDB8fDB8fHww",
+    title : "Speed",
+    description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis rerum dolores non aut eius quasi dolorum accusamus hic provident fuga saepe dignissimos, aliquid ullam quas, nostrum voluptate. Dolore, neque sit!"
+  },
+  {
+    img : "/Foodybro.png",
+    title : "FoodyBro",
+    link : "https://foodybro.vercel.app",
+    description : "A comprehensive food ordering application built using the MERN stack. The app allows users to browse various food items, add them to the cart, proceed with checkout, and manage their profiles.",
+  },
+  {
+    img : "/FloatingDock.png",
+    title : "FloatingDock",
+    link : "https://floatingdock.vercel.app",
+    description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis rerum dolores non aut eius quasi dolorum accusamus hic provident fuga saepe dignissimos, aliquid ullam quas, nostrum voluptate. Dolore, neque sit!"
+  }
+]
