d468a5bfe50c7022fd2992fe7fa28fc78eea4b2d
rajputshashank003 <hariomshashank@gmail.com>
2025-09-10 23:55:27 +0530

    feat: add data-scroll animation in app bar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom';
 import { menu } from '../utils/menu';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
-import { useGSAP } from '@gsap/react';
 gsap.registerPlugin(ScrollTrigger);
 
 const AppBar = () => {
@@ -11,7 +10,7 @@ const AppBar = () => {
     const par = useRef(null);
 
     return (
-        <div ref={par} className='sm:mx-[5%] max-sm:mx-[8px] h-[42px] md:h-[62px] shadow-[0px_0px_4px] shadow-red-800/30 sticky top-[32px] sm:top-[12px] z-[999] rounded-[12px] overflow-hidden'>
+        <div data-scroll data-scroll-speed={'0.03'} ref={par} className='sm:mx-[5%] max-sm:mx-[8px] h-[42px] md:h-[62px] shadow-[0px_0px_4px] shadow-red-800/30 sticky top-[32px] sm:top-[12px] z-[999] rounded-[12px] overflow-hidden'>
             <div className="absolute top-0 left-0 h-[80%] w-12 bg-red-700"></div>
             <div className='h-full px-[12px] md:px-[22px] items-center bg-black/30 backdrop-blur-[32px] flex flex-row space-around gap-[10px] md:gap-[42px] sm:gap-[22px] '>
                 {menu.map((val) => (
